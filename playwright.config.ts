4f8c0f743d047cbd26989c4a9c1f0c208f1c16dc
vaidasian <vaidas.sieber@gmail.com>
2025-09-05 15:30:57 +0200

    fix: sync lockfile;

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   testDir: "./tests/e2e",
   timeout: 30_000,
   retries: 1,
+  reporter: "github",
   use: {
     baseURL: "http://localhost:3000",
     headless: true,
